96c99385426249f576c199c0c2b6852192d1c27b
Pramatha Bhat <pramathahbhat@gmail.com>
2024-12-05 21:43:06 -0500

    update patient form to add report

--- UI/pages/patient_form.py
+++ UI/pages/patient_form.py
@@ -2,9 +2,11 @@ import streamlit as st
 import requests
 from generate_patient_report_pdf import create_pdf
 from requests.auth import HTTPBasicAuth
+import time
 
 base_api_url = "http://34.170.255.245"
-airflow_url = "http://34.123.143.96:8080/api/v1/dags/data_pipeline/dagRuns"
+airflow_base_url = "http://34.123.143.96:8080/api/v1"
+dag_id = "data_pipeline"
 
 def send_patient_details(payload):
     api_url = f"{base_api_url}/api/v1/patient_details"
@@ -17,16 +19,18 @@ def send_patient_details(payload):
     except Exception as e:
         return False, f"An error occurred: {str(e)}"
 
+
 def send_patient_symptoms(payload):
     api_url = f"{base_api_url}/api/v1/patient_symptoms"
     try:
         response = requests.post(api_url, json=payload)
         if response.status_code == 200 or response.status_code == 201:
-            return True, "Symptoms submitted successfully!"
+            return True
         else:
             return False, f"Failed to submit symptoms. Error: {response.text}"
     except Exception as e:
         return False, f"An error occurred: {str(e)}"
+
 
 def trigger_airflow_dag(patient_id):
     payload = {
@@ -34,11 +38,51 @@ def trigger_airflow_dag(patient_id):
             "patient_id": patient_id
         }
     }
-    response = requests.post(airflow_url, json=payload, auth=HTTPBasicAuth('admin', 'admin'))
+
+    # Trigger the DAG
+    response = requests.post(f"{airflow_base_url}/dags/{dag_id}/dagRuns", json=payload,
+                             auth=HTTPBasicAuth('admin', 'admin'))
+
     if response.status_code == 200:
-        st.success(f"DAG triggered successfully for Patient ID: {patient_id}")
+        dag_run_id = response.json()['dag_run_id']
+        st.success(f"Generating report...")
+
+        # Poll the DAG run status
+        while True:
+            run_status_response = requests.get(
+                f"{airflow_base_url}/dags/{dag_id}/dagRuns/{dag_run_id}",
+                auth=HTTPBasicAuth('admin', 'admin')
+            )
+            run_status = run_status_response.json()['state']
+            last_task_id = "generate_model_response_task"
+            if run_status == 'success':
+                st.success(f"Generating report successfully")
+                break
+            elif run_status == 'failed':
+                st.error(f"DAG run failed for Patient ID: {patient_id}")
+                break
+            else:
+                time.sleep(10)
+
+        # Fetch the logs of the last task
+        task_log_response = requests.get(
+            f"{airflow_base_url}/dags/{dag_id}/dagRuns/{dag_run_id}/taskInstances/{last_task_id}/logs/1",
+            auth=HTTPBasicAuth('admin', 'admin')
+        )
+
+        if task_log_response.status_code == 200:
+            task_log = task_log_response.text
+            start_index = task_log.find("Returned value was:")
+            if start_index != -1:
+                task_log = task_log[start_index + len("Returned value was:"):]
+            task_log = "\n".join(task_log.splitlines()[:-1])
+            return task_log
+        else:
+            return f"Failed to fetch last task logs. Status code: {task_log_response.status_code}"
+
     else:
-        st.error(f"Failed to trigger DAG. Status code: {response.status_code}, Response: {response.text}")
+        return f"Failed to trigger DAG. Status code: {response.status_code}, Response: {response.text}"
+
 
 def render():
     st.title("Welcome to MedScript")
@@ -73,48 +117,51 @@ def render():
         submitted = st.form_submit_button("Submit")
 
     if submitted:
-            if not (first_name and last_name and gender != "Select" and blood_type != "Select" and dob and contact_number and address and height and weight and symptoms and severity):
-                st.error("Please fill out all required fields correctly")
-            else:
-                patient_payload = {
-                    "FirstName": first_name,
-                    "LastName": last_name,
-                    "DateOfBirth": dob.isoformat(),
-                    "Gender": gender,
-                    "ContactNumber": contact_number,
-                    "Email": email,
-                    "Address": address,
-                    "Height": height,
-                    "Weight": weight,
-                    "BloodType": blood_type if blood_type != "Select" else None
+        if not (
+                first_name and last_name and gender != "Select" and blood_type != "Select" and dob and contact_number and address and height and weight and symptoms and severity):
+            st.error("Please fill out all required fields correctly")
+        else:
+            patient_payload = {
+                "FirstName": first_name,
+                "LastName": last_name,
+                "DateOfBirth": dob.isoformat(),
+                "Gender": gender,
+                "ContactNumber": contact_number,
+                "Email": email,
+                "Address": address,
+                "Height": height,
+                "Weight": weight,
+                "BloodType": blood_type if blood_type != "Select" else None
+            }
+
+            success, message = send_patient_details(patient_payload)
+            if success:
+                patient_id = message
+                st.success(f"Patient details submitted successfully! Patient ID: {patient_id}")
+
+                symptoms_payload = {
+                    "PatientID": patient_id,
+                    "SymptomDescription": symptoms,
+                    "Severity": severity,
+                    "AssociatedConditions": f"Existing Conditions: {existing_conditions}, Allergies: {allergies}, Medications: {current_medications}",
                 }
 
-                success, message = send_patient_details(patient_payload)
+                success, message = send_patient_symptoms(symptoms_payload)
                 if success:
-                    patient_id = message
-                    st.success(f"Patient details submitted successfully! Patient ID: {patient_id}")
+                    st.success(message)
+                    task_log = trigger_airflow_dag(patient_id)  # Trigger the Airflow DAG with the patient_id
 
-                    symptoms_payload = {
-                        "PatientID": patient_id,
-                        "SymptomDescription": symptoms,
-                        "Severity": severity,
-                        "AssociatedConditions": f"Existing Conditions: {existing_conditions}, Allergies: {allergies}, Medications: {current_medications}",
-                    }
-
-                    success, message = send_patient_symptoms(symptoms_payload)
-                    if success:
-                        st.success(message)
-                        trigger_airflow_dag(patient_id)  # Trigger the Airflow DAG with the patient_id
-                    else:
-                        st.error(message)
+                    # Display the task log in the Diagnostic Report section
+                    st.markdown("---")
+                    st.header("Diagnostic Report")
+                    st.markdown(task_log, height=1000, disabled=True)
                 else:
                     st.error(message)
+            else:
+                st.error(message)
 
     st.markdown("---")
-    st.header("Diagnostic Report")
-
-    primary_diagnosis = "Primary Diagnosis: Example diagnosis text goes here."
-    st.text(primary_diagnosis)
+    st.header("Download Diagnostic Report")
 
     if st.button("Download Diagnostic Report"):
         try:
@@ -128,5 +175,6 @@ def render():
         except Exception as e:
             st.error(f"An error occurred: {e}")
             st.write("Diagnostic report is not yet available. Please contact your healthcare provider.")
+
 
 render()
